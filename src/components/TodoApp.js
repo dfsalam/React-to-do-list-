4b105b66e3c3299d870fdf10d320f8a860cf250a
dfsalam <dfsalam@gmail.com>
2023-02-20 22:10:30 -0500

    Part 1-2 - Setup application - Add components

---
+++ src/components/TodoApp.js
@@ -0,0 +1,14 @@
+import '../App.css';
+import Header from './Header.jsx'
+import TodosLogic from './TodosLogic';
+const TodoApp = () => {
+  return (
+    <div>
+      <Header />
+      <TodosLogic />      
+    </div>
+  );
+};
+export default TodoApp;
+
+
